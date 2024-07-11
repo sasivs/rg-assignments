86a322c5959d60b63b5b9fcc715bf49567939cba
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2024-07-11 14:55:26 +0530

    Date and Time API - Week 4

---
+++ src/LocalDateTimeExample.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class LocalDateTimeExample {
+    public static void main(String[] args) {
+        LocalDateTime d = LocalDateTime.now();
+        System.out.println("Current Date and Time: "+d);
+    }
+}
